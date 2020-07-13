0b74bed9290dc0f54c82509ed65eeafc5f9dec41
Markus Haldorsen <markushaldorsen@gmail.com>
2020-07-13 16:21:08 +0200

    Initial commit

---
+++ src/service.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/futurehomeno/fimpgo"
+	"github.com/futurehomeno/fimpgo/discovery"
+	log "github.com/sirupsen/logrus"
+	"github.com/thingsplex/sonos/model"
+	"github.com/thingsplex/sonos/router"
+	"github.com/thingsplex/sonos/utils"
+)
+
+func main() {
+	var workDir string
+	flag.StringVar(&workDir, "c", "", "Work dir")
+	flag.Parse()
+	if workDir == "" {
+		workDir = "./"
+	} else {
+		fmt.Println("Work dir ", workDir)
+	}
+	appLifecycle := model.NewAppLifecycle()
+	configs := model.NewConfigs(workDir)
+	err := configs.LoadFromFile()
+	if err != nil {
+		fmt.Print(err)
+		panic("Can't load config file.")
+	}
+
+	utils.SetupLog(configs.LogFile, configs.LogLevel, configs.LogFormat)
+	log.Info("--------------Starting sonos----------------")
+	log.Info("Work directory : ", configs.WorkDir)
+	appLifecycle.PublishEvent(model.EventConfiguring, "main", nil)
+
+	mqtt := fimpgo.NewMqttTransport(configs.MqttServerURI, configs.MqttClientIdPrefix, configs.MqttUsername, configs.MqttPassword, true, 1, 1)
+	err = mqtt.Start()
+	responder := discovery.NewServiceDiscoveryResponder(mqtt)
+	responder.RegisterResource(model.GetDiscoveryResource())
+	responder.Start()
+
+	fimpRouter := router.NewFromFimpRouter(mqtt, appLifecycle, configs)
+	fimpRouter.Start()
+
+	for {
+		appLifecycle.WaitForState("main", model.AppStateRunning)
+		// Configure custom resources here
+		//if err := conFimpRouter.Start(); err !=nil {
+		//	appLifecycle.PublishEvent(model.EventConfigError,"main",nil)
+		//}else {
+		//	appLifecycle.WaitForState(model.StateConfiguring,"main")
+		//}
+		//TODO: Add logic here
+		appLifecycle.WaitForState(model.AppStateNotConfigured, "main")
+	}
+
+	mqtt.Stop()
+	time.Sleep(5 * time.Second)
+}
---
+++ src/sonos-api/client.go
@@ -0,0 +1,98 @@
+package sonos
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	controlURL = "https://api.ws.sonos.com/control/api"
+)
+
+type Config struct {
+	ErrorCode  int    `json:"errorCode"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"statusCode"`
+	Success    bool   `json:"success"`
+}
+
+type Client struct {
+	httResponse *http.Response
+	Households  []Household `json:"households"`
+	Groups      []Group     `json:"groups"`
+	Players     []Player    `json:"players"`
+}
+
+type Household struct {
+	ID string `json:"id"`
+}
+
+type Group struct {
+	GroupId       string        `json:"id"`
+	Name          string        `json:"name"`
+	CoordinatorId string        `json:"coordinatorId"`
+	PlaybackState string        `json:"playbackState"`
+	PlayersIds    []interface{} `json:"playerIds"`
+}
+
+type Player struct {
+	Id            string        `json:"id"`
+	Name          string        `json:"name"`
+	Icon          string        `json:"icon"`
+	SWVersion     string        `json:"softwareVersion"`
+	DeviceIds     []interface{} `json:"deviceIds"`
+	APIVersion    string        `json:"apiVersion"`
+	MinAPIVersion string        `json:"minApiVersion"`
+	Capabilities  []interface{} `json:"capabilities"`
+}
+
+func (c *Client) GetHousehold(accessToken string) (*Client, error) {
+	url := fmt.Sprintf("%s%s", controlURL, "v1/households")
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Error(fmt.Errorf("Can't get households, error: ", err))
+	}
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Authorization", os.ExpandEnv(fmt.Sprintf("%s%s", "Bearer ", accessToken)))
+	resp, err := http.DefaultClient.Do(req)
+	processHTTPResponse(resp, err, c)
+
+	return c, nil
+}
+
+func (c *Client) GetPlayersAndGroups(accessToken string, HouseholdID string) (*Client, error) {
+	url := fmt.Sprintf("%s%s%s%s", controlURL, "v1/households", HouseholdID, "groups")
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Error(fmt.Errorf("Can't get players and groups, error: ", err))
+	}
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Authorization", os.ExpandEnv(fmt.Sprintf("%s%s", "Bearer ", accessToken)))
+	resp, err := http.DefaultClient.Do(req)
+}
+
+func processHTTPResponse(resp *http.Response, err error, holder interface{}) error {
+	if err != nil {
+		log.Error(fmt.Errorf("API does not respond"))
+		return err
+	}
+	defer resp.Body.Close()
+	// check http return code
+	if resp.StatusCode != 200 {
+		//bytes, _ := ioutil.ReadAll(resp.Body)
+		log.Error("Bad HTTP return code ", resp.StatusCode)
+		return fmt.Errorf("Bad HTTP return code %d", resp.StatusCode)
+	}
+
+	// Unmarshall response into given struct
+	if err = json.NewDecoder(resp.Body).Decode(holder); err != nil {
+		return err
+	}
+	return nil
+}
