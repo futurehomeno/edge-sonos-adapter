6c170a5ec735acbf02f8874b01b045e8f91e1e55
Vlad Datcu <vlad.datcu@gmail.com>
2020-08-14 14:51:04 +0200

    - error logging - reuse instance of fhOauthClient

--- src/go.mod
+++ src/go.mod
@@ -3,7 +3,10 @@ module github.com/thingsplex/sonos
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/futurehomeno/fimpgo v1.5.4
+	github.com/pkg/errors v0.0.0-20161029093637-248dadf4e906
 	github.com/sirupsen/logrus v1.3.0
+	github.com/stretchr/testify v1.6.1
+	github.com/thoas/go-funk v0.7.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- src/service.go
+++ src/service.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
+	"github.com/pkg/errors"
+	"github.com/thoas/go-funk"
 	"reflect"
+	"time"
 
 	"github.com/futurehomeno/fimpgo"
 	"github.com/futurehomeno/fimpgo/discovery"
@@ -19,25 +21,24 @@ func main() {
 	var workDir string
 	flag.StringVar(&workDir, "c", "", "Work dir")
 	flag.Parse()
-	if workDir == "" {
+	if funk.IsEmpty(workDir) {
 		workDir = "./"
 	} else {
 		fmt.Println("Work dir ", workDir)
 	}
+
 	appLifecycle := model.NewAppLifecycle()
 	configs := model.NewConfigs(workDir)
 	states := model.NewStates(workDir)
 	err := configs.LoadFromFile()
 	if err != nil {
-		fmt.Print(err)
-		panic("Can't load config file.")
+		log.Fatal(errors.Wrap(err, "can't load config file."))
 	}
 	err = states.LoadFromFile()
 	if err != nil {
-		fmt.Print(err)
-		panic("Can't load state file.")
+		log.Fatal(errors.Wrap(err, "can't load state file."))
 	}
-	client := sonos.Client{}
+	client := sonos.NewClient(configs.Env)
 
 	edgeapp.SetupLog(configs.LogFile, configs.LogLevel, configs.LogFormat)
 	log.Info("--------------Starting sonos----------------")
@@ -46,6 +47,8 @@ func main() {
 
 	mqtt := fimpgo.NewMqttTransport(configs.MqttServerURI, configs.MqttClientIdPrefix, configs.MqttUsername, configs.MqttPassword, true, 1, 1)
 	err = mqtt.Start()
+	defer mqtt.Stop()
+
 	responder := discovery.NewServiceDiscoveryResponder(mqtt)
 	responder.RegisterResource(model.GetDiscoveryResource())
 	responder.Start()
@@ -74,24 +77,33 @@ func main() {
 		ticker := time.NewTicker(time.Duration(15) * time.Second)
 		var oldReport map[string]interface{}
 		var oldPbStateValue string
-		var oldPlayModes struct{ Repeat bool "json:\"repeat\""; RepeatOne bool "json:\"repeatOne\""; Shuffle bool "json:\"shuffle\""; Crossfade bool "json:\"crossfade\"" }
+		var oldPlayModes struct {
+			Repeat    bool `json:"repeat"`
+			RepeatOne bool `json:"repeatOne"`
+			Shuffle   bool `json:"shuffle"`
+			Crossfade bool `json:"crossfade"`
+		}
 		var oldVolume int
 		var oldMuted bool
-		for ; true; <-ticker.C {
+
+		for range ticker.C {
 			if configs.AccessToken != "" && configs.AccessToken != "access_token" {
 				// ADD LOGIC TO HANDLE REFRESH TOKEN
 				// every 24 hours at least
 				// if millis is more than 12 hours after last authorization, make new
-				millis := time.Now().UnixNano() / 1000000
+				currentMillis := time.Now().UnixNano() / 1000000
 				refreshMillis := configs.LastAuthMillis + 43200000
-				if millis > refreshMillis {
-					newAccessToken, err := client.RefreshAccessToken(configs.RefreshToken, configs.MqttServerURI, configs.Env)
+
+				if currentMillis > refreshMillis {
+					newAccessToken, err := client.RefreshAccessToken(configs.RefreshToken, configs.MqttServerURI)
 					if err != nil {
-						log.Error(err)
+						log.Error(errors.Wrap(err, "refreshing access token"))
 					}
 					configs.AccessToken = newAccessToken
-					configs.LastAuthMillis = millis
-					configs.SaveToFile()
+					configs.LastAuthMillis = currentMillis
+					if err := configs.SaveToFile(); err != nil {
+						log.Error(err)
+					}
 				}
 			}
 			for i := 0; i < len(configs.WantedHouseholds); i++ {
@@ -131,7 +143,9 @@ func main() {
 					oldReportEqualsNewReport := reflect.DeepEqual(oldReport, report)
 					if !oldReportEqualsNewReport {
 						msg := fimpgo.NewMessage("evt.metadata.report", "media_player", fimpgo.VTypeStrMap, report, nil, nil, nil)
-						mqtt.Publish(adr, msg)
+						if err := mqtt.Publish(adr, msg); err != nil {
+							log.Error(err)
+						}
 						oldReport = report
 						log.Info("New metadata message sent to fimp")
 					}
@@ -154,32 +168,40 @@ func main() {
 					pbStateValue := client.SetCorrectValue(states.PlaybackState)
 					if oldPbStateValue != pbStateValue {
 						msg := fimpgo.NewMessage("evt.playback.report", "media_player", fimpgo.VTypeString, pbStateValue, nil, nil, nil)
-						mqtt.Publish(adr, msg)
+						if err := mqtt.Publish(adr, msg); err != nil {
+							log.Error(err)
+						}
 						oldPbStateValue = pbStateValue
 						log.Info("New playback.report sent to fimp")
 					}
 					oldPlayModesEqualsNewPlaymodes := reflect.DeepEqual(oldPlayModes, states.PlayModes)
 					playmodes := map[string]bool{
-						"repeat": states.PlayModes.Repeat,
+						"repeat":     states.PlayModes.Repeat,
 						"repeat_one": states.PlayModes.RepeatOne,
-						"shuffle": states.PlayModes.Shuffle,
-						"crossfade": states.PlayModes.Crossfade,
+						"shuffle":    states.PlayModes.Shuffle,
+						"crossfade":  states.PlayModes.Crossfade,
 					}
 					if !oldPlayModesEqualsNewPlaymodes {
 						msg := fimpgo.NewMessage("evt.playbackmode.report", "media_player", fimpgo.VTypeBoolMap, playmodes, nil, nil, nil)
-						mqtt.Publish(adr, msg)
+						if err := mqtt.Publish(adr, msg); err != nil {
+							log.Error(err)
+						}
 						oldPlayModes = states.PlayModes
 						log.Info("New playbackmode.report sent to fimp")
 					}
 					if oldVolume != states.Volume {
 						msg := fimpgo.NewMessage("evt.volume.report", "media_player", fimpgo.VTypeInt, states.Volume, nil, nil, nil)
-						mqtt.Publish(adr, msg)
+						if err := mqtt.Publish(adr, msg); err != nil {
+							log.Error(err)
+						}
 						oldVolume = states.Volume
 						log.Info("New volume.report sent to fimp")
 					}
 					if oldMuted != states.Muted {
 						msg := fimpgo.NewMessage("evt.mute.report", "media_player", fimpgo.VTypeBool, states.Muted, nil, nil, nil)
-						mqtt.Publish(adr, msg)
+						if err := mqtt.Publish(adr, msg); err != nil {
+							log.Error(err)
+						}
 						oldMuted = states.Muted
 						log.Info("New mute.report sent to fimp")
 					}
@@ -189,11 +211,11 @@ func main() {
 				}
 			}
 			log.Debug("ticker")
-			states.SaveToFile()
+			if err := states.SaveToFile(); err != nil {
+				log.Error(err)
+			}
 		}
 		appLifecycle.WaitForState(model.AppStateNotConfigured, "main")
 	}
 
-	mqtt.Stop()
-	time.Sleep(5 * time.Second)
 }
